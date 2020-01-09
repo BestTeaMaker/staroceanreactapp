4ca2bd0f7103c299e655bc6d9da7f052b931ebdc
BestTeaMaker <thebestteamaker@gmail.com>
2020-01-09 13:09:04 -0800

    Updates

---
+++ precache-manifest.f6fe64ab3d28c4f84f649b033228f6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02181f1276c7e5ed66163bdfedf10a9b",
+    "url": "/staroceanreactapp/index.html"
+  },
+  {
+    "revision": "09fa53537197cb087c74",
+    "url": "/staroceanreactapp/static/css/main.ce4e3a04.chunk.css"
+  },
+  {
+    "revision": "b9e531fbd91ab63f5786",
+    "url": "/staroceanreactapp/static/js/2.ffaf4656.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/staroceanreactapp/static/js/2.ffaf4656.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09fa53537197cb087c74",
+    "url": "/staroceanreactapp/static/js/main.e17bb62e.chunk.js"
+  },
+  {
+    "revision": "38a73ddb58b854add175",
+    "url": "/staroceanreactapp/static/js/runtime-main.742bcb76.js"
+  }
+]);
